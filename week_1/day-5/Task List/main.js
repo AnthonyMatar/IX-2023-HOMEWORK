cd9df7d272c71d341094f4ef7e8b180ec7aadb57
Anthony Matar <xgh8sv@virginia.edu>
2023-06-02 17:28:03 +0200

    Started task manager

---
+++ week_1/day-5/Task List/main.js
@@ -0,0 +1,30 @@
+class Task{
+    constructor(task){
+        this.task = task;
+    }
+}
+
+class UI{
+    cosntructor(){
+        this.form = docoument.getElementById('form');
+
+        this.task = document.getElementById('task-input');
+
+
+        this.tableBody = document.getElementById('table-body');
+
+        this.form.addEventListener('submit', (e)=> this.onFormSubmit(e));
+
+    }
+
+    onFormSubmit(e){
+        e.preventDefault();
+        console.log("Hello");
+    }
+}
+
+
+
+
+
+const ui = new UI();
