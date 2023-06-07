18b09b7c544e96dc680d0d4ccd1f59e33d89bc91
Anthony Matar <xgh8sv@virginia.edu>
2023-06-07 20:43:26 +0200

    Created project

---
+++ week_2/day_8/firebase_practice/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="text-center"> Hello
+    </div>
+  );
+}
+
+export default App;
